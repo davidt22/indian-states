931bd3e0b950313eedd7ebbc5494527037a46b6c
David Teruel <d_teruel@hotmail.com>
2015-09-22 12:21:14 +0200

    Created a list view of indian states and action to download the csv file

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Reader\Reader;
+use Doctrine\Common\Collections\ArrayCollection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,8 +17,52 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('default/index.html.twig', array());
     }
+
+
+    /**
+     * @Route("/list", name="list-indian-states")
+     */
+    public function listIndianStatesAction(Request $request)
+    {
+        $rootDir = $this->get('kernel')->getRootDir() . '/Resources/fixtures/';
+        $csvReader = new Reader($rootDir);
+
+        $dataFixtures = $csvReader->readCSV();
+
+        $indianStatesCollection = $csvReader->fillStatesWithData($dataFixtures);
+
+        return $this->render('default/list.html.twig', array(
+            'states' => $indianStatesCollection
+            )
+        );
+    }
+
+    /**
+     * @Route("/download-file", name="download-file")
+     */
+    public function downloadFileAction(Request $request)
+    {
+        // Generate response
+        $response = new Response();
+
+        $filename = $this->get('kernel')->getRootDir() . '/Resources/fixtures/list-indian-states.csv';
+
+        // Set headers
+        $response->headers->set('Cache-Control', 'private');
+        $response->headers->set('Content-type', mime_content_type($filename));
+        $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($filename) . '";');
+        $response->headers->set('Content-length', filesize($filename));
+
+        // Send headers before outputting anything
+        $response->sendHeaders();
+
+        $response->setContent(file_get_contents($filename));
+
+        return $response;
+
+    }
+
+
 }
---
+++ src/AppBundle/Reader/Reader.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Reader;
+
+use AppBundle\Entity\State;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\File;
+
+class Reader
+{
+    // change these options about the file to read
+    private $csvParsingOptions = array(
+        'finder_in' => __DIR__,
+        'finder_name' => 'list-indian-states.csv'
+    );
+
+    /**
+     * Reader constructor.
+     */
+    public function __construct($rootDir)
+    {
+        $this->csvParsingOptions['finder_in'] = $rootDir;
+    }
+
+
+    /**
+     * Read a csv file and return it to an array
+     *
+     * @return array
+     */
+    public function readCSV()
+    {
+        $finder = new Finder();
+        $finder->files()
+            ->in($this->csvParsingOptions['finder_in'])
+            ->name($this->csvParsingOptions['finder_name'])
+        ;
+
+        foreach ($finder as $file) {
+            /** @var File $csv */
+            $csv = $file;
+        }
+
+        $rows = array();
+        if (($handle = fopen($csv->getRealPath(), "r")) !== FALSE) {
+            $i = 0;
+            while (($data = fgetcsv($handle, null, ",")) !== FALSE) {
+                $i++;
+
+                if(is_numeric($data[0])){
+                    $rows[] = $data;
+                }
+
+            }
+
+            fclose($handle);
+        }
+
+        return $rows;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    public function fillStatesWithData(array $data)
+    {
+        $listIndianStates = array();
+
+        foreach ($data as $state){
+            $indianState = new State();
+            $indianState->setId($state[0]);
+            $indianState->setName($state[1]);
+            $indianState->setPopulation($state[2]);
+            $indianState->setArea($state[3]);
+            $indianState->setOfficial($state[4]);
+            $indianState->setCapital($state[5]);
+            $indianState->setLargestCity($state[6]);
+            $indianState->setPopulationDensity($state[7]);
+            $indianState->setLiteracyRate($state[8]);
+            $indianState->setPercentajeUrbanPopulation($state[9]);
+            $indianState->setSexRatio($state[10]);
+
+            array_push($listIndianStates, $indianState);
+        }
+
+        return $listIndianStates;
+    }
+
+}
